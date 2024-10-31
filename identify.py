216580f1b7b12898e8e2b9ca2d26d3024bf4b9df
girlanasouza <girlanasouza123@gmail.com>
2024-10-31 14:53:52 -0400

    identificar quadro

--- identify.py
+++ identify.py
@@ -1,12 +1,17 @@
 import cv2
+import os
 
 # Carrega o vídeo
-video_path = 'input/v10.mp4'
+video_path = 'input/v9.mp4'
 video = cv2.VideoCapture(video_path)
+
+# Cria o diretório de saída se não existir
+output_dir = 'output/v9'
+os.makedirs(output_dir, exist_ok=True)
 
 # Parâmetros
 frame_rate = int(video.get(cv2.CAP_PROP_FPS))  # Quadros por segundo do vídeo
-frame_interval = frame_rate * 2  # Extrai um quadro a cada 2 segundos
+frame_interval = frame_rate * 1  # Extrai um quadro a cada 2 segundos
 
 # Contador de quadros
 frame_count = 0
@@ -18,7 +23,9 @@ while video.isOpened():
     
     # Salva o quadro a cada 'frame_interval'
     if frame_count % frame_interval == 0:
-        cv2.imwrite(f'frame_{frame_count}.jpg', frame)
+        frame_path = os.path.join(output_dir, f'frame_{frame_count}.jpg')
+        cv2.imwrite(frame_path, frame)
+    
     
     frame_count += 1
 
